694b772074bb57f4e018ae0bbbd052e941672e98
mr-olabi <your-email@example.com>
2024-07-27 12:13:49 +0300

    optimized

--- js/script.js
+++ js/script.js
@@ -1,30 +1,12 @@
 import Aos from "aos";
-import 'aos/dist/aos.css'
+import 'aos/dist/aos.css';
 
-// init Aos animation 
+// Initialize AOS animation
 Aos.init({
-    duration :1000,
+    duration: 1000,
     offset: 100,
 });
 
-// navbar stick on the top while scrolling 
-
-const nav = document.getElementById('nav');
-
-// Get the offset position of the navigation element
-const sticky = nav.offsetTop;
-
-// Function to add or remove the "sticky" class based on scroll position
-function checkStickyNav() {
-  if (window.pageYOffset >= sticky) {
-    nav.classList.add('sticky');
-  } else {
-    nav.classList.remove('sticky');
-  }
-}
-
-// Add the scroll event listener to check the sticky nav on scroll
-window.addEventListener('scroll', checkStickyNav);
 
 
 const trendingSushis = [
